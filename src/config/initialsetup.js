c3b057ae77b16ff823be9f119c17be3d1dd431f9
matias-vilchesb <matias.75vilches@gmail.com>
2025-09-17 14:16:28 -0300

    configuracion .env y initialsetup

---
+++ src/config/initialsetup.js
@@ -0,0 +1,41 @@
+"use strict";
+import bcrypt from "bcryptjs";
+import { AppDataSource } from "./configDb.js";
+import User from "../entities/user.entity.js"
+
+
+async function encryptPassword(password){
+    const saltRounds = 10;
+    return await bcrypt.hash(password,saltRounds);
+}
+
+export async function createUser() {
+    try{
+        const userRepository = AppDataSource.getRepository(User);
+        const count = await userRepository.count();
+        if (count > 0)return;
+    
+        const now = new Date();
+
+        
+        //Crear usuarios iniciales
+        await Promise.all([
+        userRepository.save(userRepository.create({
+            email: "admin@prueba.com",
+            password: await encryptPassword("admin123"),
+            estado_activo: true,
+            fecha_registro: now,
+
+        })),
+        userRepository.save(userRepository.create({
+            email: "demo1@gmail.com",
+            password: await encryptPassword("demo1"),
+            estado_activo: true,
+            fecha_registro: now,
+        })),
+    ]);
+    console.log("* Usuarios creados exitosamente");
+    } catch (error){
+        console.error("error al crear usuarios", error);
+    }
+}
